60fcc9b98a65e8ae8f71286c018420a1c20c0765
fullgrown31 <dwightmopas@ymail.com>
2019-02-10 23:22:48 -0600

    Created search functionality

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,4 +1,7 @@
 class SearchController < ApplicationController
   def results
+    @query = params[:q]
+    @games_count = Game.where('name LIKE ?', "%#{@query}%")
+    @games = @games_count.includes(:ratings).order(:name).paginate(:page => params[:page], :per_page => 30)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,22 @@
 Rails.application.routes.draw do
-  get 'search/results'
-  get 'ratings/index'
-  get 'ratings/show'
-  get 'games/index'
-  get 'games/show'
-  get 'users/index'
-  get 'users/show'
+  get 'static_pages/home'
+  get 'static_pages/about'
+  get 'static_pages/contact'
   resources :pages
   resources :platforms, only: [:index, :show]
   resources :genres, only: [:index, :show]
   resources :users, only: [:index, :show]
   resources :games, only: [:index, :show]
 
+  get 'search_results', to:'search#results', as: 'search_results'
+
   get 'static/:permalink', to: 'pages#static', as: 'static'
-  root 'games#index'
+
+  get 'static_pages/home'
+  root 'static_pages#home'
+
+  get 'static_pages/about'
+  get 'static_pages/contact'
   # root 'pages#show', {id: 3}
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
